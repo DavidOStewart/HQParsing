f26d2a02c59915b252fd493b8ebc49850844a4d3
Tim Wright <spartas@gmail.com>
2018-05-21 22:43:28 -0500

    Added WebSocket listening to poll_hq.py

--- src/poll_hq.py
+++ src/poll_hq.py
@@ -4,25 +4,57 @@ import ConfigParser
 import json
 import sys
 
+from ws4py.client.threadedclient import WebSocketClient
+
+class DummyClient(WebSocketClient):
+  def closed(self, code, reason=None):
+    print("Closed down", code, reason)
+
+  def received_message(self, m):
+
+    msg = json.loads(str(m))
+    
+    if 'type' in msg:
+      if 'broadcastEnded' == msg['type']:
+        self.close(reason='Broadcast Ended')
+
+      elif 'question' == msg['type']:
+        print("QUESTION:")
+        print(msg['question'], msg['answers'])
+        print("\n")
+
 config = ConfigParser.RawConfigParser()
 config.read('hq.ini')
 
-bearer_token = config.get('Main', 'bearer_token')
-
-#print(bearer_token)
-#sys.exit(1)
-
 url='https://api-quiz.hype.space/shows/now'
 h = {
-    'User-Agent':'HQ-iOS/88 CFNetwork/808.2.16 Darwin/16.3.0',
+    'User-Agent': config.get('Main', 'user-agent'),
     'x-hq-stk':'MQ==',
-    'x-hq-device':'iPhone8,1',
+    'x-hq-device': config.get('Main', 'x-hq-device'),
     'Accept':'*/*',
     'Accept-Language':'en-us',
-    'x-hq-client':'iOS/1.3.5 b88',
+    'x-hq-client': config.get('Main', 'x-hq-client'),
     'x-hq-test-key':'',
-    'Authorization':'Bearer %s' % (bearer_token,)
+    'Authorization':'Bearer %s' % config.get('Main', 'bearer_token'),
 }
 
-print(requests.get(url, data={'type':'hq'}, headers=h).json())
+response = requests.get(url, data={'type':'hq'}, headers=h).json()
+
+if not response['broadcast']:
+  print("No broadcast started.")
+  sys.exit(0)
+
+
+try:
+  url = response['socketUrl'].replace('https', 'wss')
+  h = {
+    'User-Agent' : config.get('Main', 'user-agent'),
+    'Authorization':'Bearer %s' % config.get('Main', 'bearer_token'),
+    'x-hq-client' : config.get('Main', 'x_hq_client'),
+  }
+  ws = DummyClient(url, headers=h)
+  ws.connect()
+  ws.run_forever()
+except KeyboardInterrupt:
+  ws.close()
 
